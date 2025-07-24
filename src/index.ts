65c8cf425923a5d753454b0182307253d69f5931
Ddilibe <franklinfidelugwuowo@gmail.com>
2025-07-24 15:07:42 +0100

    [Chore]: Remove the list endpoints function

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import swaggerUi from 'swagger-ui-express';
 import mainRouter from './routes';
 import { AppDataSource } from './config/database';
 import { specs } from './config/swagger';
-import listEndpoints from 'express-list-endpoints';
 
 dotenv.config();
 
@@ -32,12 +31,5 @@ AppDataSource.initialize()
   .catch((error) => {
     console.error('Error during Data Source initialization:', error);
   });
-
-const endpoints = listEndpoints(app);
-
-console.log('Registered Endpoints: ', endpoints);
-endpoints.forEach(endpoint => {
-  console.log(`${endpoint.methods.join(', ')} ${endpoint.path}`);
-});
 
 export default app; 
