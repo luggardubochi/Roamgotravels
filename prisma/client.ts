f3a48b2a07aa7878634ca37e611f1793cfb28aa5
Ddilibe <franklinfidelugwuowo@gmail.com>
2025-08-22 04:17:41 +0100

    [Chore]: Upgraded the database schema of the project

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "@/app/generated/prisma";
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient
