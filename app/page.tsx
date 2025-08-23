42a38f1f527f6f0a90371197f6b3eb99cfde641c
Ddilibe <franklinfidelugwuowo@gmail.com>
2025-08-23 04:18:26 +0100

    [Chore]: Updated page design

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import TravelExperience from "./components/TravelExperience";
 import SuggestedDestinations from "./components/SuggestedDestination";
 import GroupDestinations from "./components/GroupDestination";
+import TripOptions from "./components/Browse";
 
 export default function Home() {
   return (
     <>
+    <TripOptions />
     <TravelExperience />
     <SuggestedDestinations />
     <GroupDestinations />
