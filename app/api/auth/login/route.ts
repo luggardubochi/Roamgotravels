080f4e2f10726632f751410526d000d1fc7c8faf
Ddilibe <franklinfidelugwuowo@gmail.com>
2025-08-27 04:23:20 +0100

    [Chore]: Started working on the API

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/prisma/client";
+import { createJWT } from "@/lib/auth";
+import bcrypt from "bcryptjs";
+import { TOKENNAME } from "@/app/Constants";
+import { cookies } from "next/headers";
+
+
+
+export async function POST(req: Request) {
+    try {
+        const { email, password } = await req.json();
+
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+        }
+
+        // Check password
+        const match = await bcrypt.compare(password, user.password as string);
+        if (!match) {
+            return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+        }
+
+        // Generate JWT
+        const token = await createJWT(user);
+
+        const res = NextResponse.json({ message: "Login successful" }, { status: 200 });
+        const cookiestore = await cookies();
+        console.log(cookiestore);
+        cookiestore.set(TOKENNAME, token, {
+            httpOnly: true,
+            path: "/",
+            secure: true,
+            maxAge: 60 * 60 * 24,
+        });
+        return res
+    } catch (err) {
+
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import bcrypt from "bcryptjs";
+import { v4 as uuidv4 } from 'uuid';
+
+export async function POST(req: Request) {
+    try {
+        const { name, email, password } = await req.json();
+
+        if (!name || !email || !password) {
+            return NextResponse.json({ error: "Missing fields" }, { status: 400 });
+        }
+
+        // Check if user exists
+        const existing = await prisma.user.findUnique({ where: { email } });
+        if (existing) {
+            return NextResponse.json({ error: "User already exists" }, { status: 409 });
+        }
+
+        // Hash password
+        const hashed = await bcrypt.hash(password, 10);
+
+        // Save user
+        const data = {
+            id: uuidv4(), name: name, email: email,
+            password: hashed, role: "user",
+            emailVerified: false, image: null,
+        };
+
+        const user = await prisma.user.create({
+            data: data,
+        });
+
+        return NextResponse.json(
+            { message: "User created", user: { id: user.id, email: user.email } },
+            { status: 201 }
+        );
+    } catch (err) {
+        console.error("Signup error:", err);
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
+
